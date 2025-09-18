30274d922b41c56f4061f0aa6d531d06090e26d4
Bocchiid <487883857@qq.com>
2025-09-18 10:26:51 +0800

    Add 模拟游戏对话.py

---
+++ Experiment_01/模拟游戏对话/模拟游戏对话.py
@@ -0,0 +1,10 @@
+# 请补充你的代码
+
+user_name = input("请输入角色的名字：")
+user_level = int(input("请输入角色的等级："))
+user_owned_money = int(input("请输入角色拥有的金币数量："))
+
+print(f'欢迎，勇敢的 {user_name}！')
+print(f'你现在的等级是 {user_level} 级。')
+print(f'你拥有 {user_owned_money} 个金币。')
+print(f'{user_name}，继续你的冒险吧！')
