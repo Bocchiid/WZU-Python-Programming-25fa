bd59532de737d01c0e74204303b89f4c5ecfd599
Bocchiid <487883857@qq.com>
2025-09-30 15:17:23 +0800

    Add pow函数详解.py

---
+++ Experiment_04/4.2 自定义数学函数(project)/pow函数详解.py
@@ -0,0 +1,17 @@
+def pow(x, n):  # 幂运算函数
+    """接收一个数字x和一个整数n为参数,返回x的n次幂的结果的浮点数类型
+    要求使pow(1.0, x) 和 pow(x, 0.0) 总是返回 1.0"""
+    #########################Begin############################### 
+    ret = 1.0
+
+    for i in range(n):
+        ret *= x
+
+    return ret
+    #########################End############################### 
+
+if __name__ == '__main__':
+    x = float(input())
+    n = int(input())
+    return_data = pow(x,n)
+    print(return_data)
