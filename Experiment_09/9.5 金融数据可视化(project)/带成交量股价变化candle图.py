b61c11723d2d1275c62eedf4d71b4ed87d48f37c
Bocchiid <487883857@qq.com>
2025-11-23 13:52:05 +0800

    Add 带成交量股价变化candle图.py

---
+++ Experiment_09/9.5 金融数据可视化(project)/带成交量股价变化candle图.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import mplfinance as mpf
+import warnings
+import matplotlib.pyplot as plt
+
+
+def pd_read_file(filename):
+    """读取文件时，将date列读取为index，此时索引类型为obj，将索引类型更改为DatetimeIndex"""
+    df = pd.read_csv(filename, index_col = 'Date')
+    df.index = pd.DatetimeIndex(df.index)
+
+    return df
+
+
+def plot_mpf_candle(df):
+    """ df为pandas.DataFrame类型，必须包含’Open’, ‘High’, ‘Low’ 和 ‘Close’ 数据
+    绘制类型为`candle`，图中要有 2 日、5 日和 10 日均线，并设置参数 `volume=True`显示成交量"""
+    mpf.plot(
+        data = df,
+        type = 'candle',
+        ylabel = 'Price',
+        mav = (2, 5, 10),
+        volume = True
+    )    
+
+
+if __name__ == '__main__':
+    file = '600132202009.csv'  # 文件名
+    warnings.filterwarnings('ignore')  # 忽略警告
+    df_stock = pd_read_file(file)
+    plot_mpf_candle(df_stock)
+    plt.savefig('result/result.jpg')
+    plt.show() 
